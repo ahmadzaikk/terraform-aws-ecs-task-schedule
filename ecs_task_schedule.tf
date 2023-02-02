4e1616eadecb9cf84c9de9ed824ccbff2d1399c3
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-02-02 13:05:57 -0800

    Update ecs_task_schedule.tf

--- ecs_task_schedule.tf
+++ ecs_task_schedule.tf
@@ -74,6 +74,19 @@ resource "aws_iam_role_policy_attachment" "role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
 }
 
+### Task Definition
+resource "aws_ecs_task_definition" "default" {
+  count = var.enabled ? 1 : 0
+  family = join("-", [var.name, "task"]) # Naming our first task
+  execution_role_arn = aws_iam_role.execution_role.arn
+  container_definitions = var.container_definitions
+  cpu = var.cpu
+  memory = var.memory
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
+  tags = var.tags
+}
+
 ## Cloudwatch event
 resource "aws_cloudwatch_event_rule" "scheduled_task" {
   name                = join("-", [var.name, "schedule", "task"])
